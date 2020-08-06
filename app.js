90bdb9a1c0659a9dc3284415b271af12b956407b
prathikotian <prathi.kotian@outlook.com>
2020-08-06 15:44:46 +0530

    refactored routes

--- app.js
+++ app.js
@@ -1,34 +1,12 @@
 const express = require('express'),
       app = express(),
+      routes = require('./routes/routes'),
       port = process.env.port || 3000;
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public/img'));
 app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  res.render('main');
-});
-
-app.get('/blog', (req, res) => {
-  res.render('posts');
-});
-
-app.get('/p0', (req, res) => {
-  res.render('posts/post1');
-});
-
-app.get('/p1', (req, res) => {
-  res.render('posts/post2');
-});
-
-app.get('/p2', (req, res) => {
-  res.render('posts/post3');
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`Server started listening on port ${port}`);
