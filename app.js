deca6aed8422c8ee9b3e85500fa4cdbced0bef8f
prathikotian <prathi.kotian@outlook.com>
2020-08-06 16:01:55 +0530

    server config

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express'),
       app = express(),
       routes = require('./routes/routes'),
-      port = process.env.port || 3000;
+      port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public/img'));
